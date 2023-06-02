893efd9ea06f3db7c60cce38d62fa6e88fad5ff7
unknown <shivamdureja77@gmail.com>
2023-06-02 16:33:35 +0530

    Project complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import './App.css';
+import Cards from "./components/Cards";
+import Navbar from "./components/Navbar";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return(
+    <><Navbar/>
+    
+    <div className="d-flex gap-4 justify-content-center pt-5">
+    
+        <Cards  title="Javascript"
+        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=400"
+        prag="loreum10 loreum10loreum10loreum10loreum10loreum10loreum10loreum10"
+        />
+       <Cards  title="Nodejs"
+        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=400"
+        prag="loreum10 loreum10loreum10loreum10loreum10loreum10loreum10loreum10"
+        />
+       <Cards  title="Reactjs"
+        src="https://images.pexels.com/photos/5483077/pexels-photo-5483077.jpeg?auto=compress&cs=tinysrgb&w=400"
+        prag="loreum10 loreum10loreum10loreum10loreum10loreum10loreum10loreum10"
+        />
+    </div> 
+    </>
+
+  )
 }
 
 export default App;
